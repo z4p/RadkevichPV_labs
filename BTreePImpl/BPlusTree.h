1331151fd21b9060d4b5d350065519742f2c1bc5
z4p <z4p@tut.by>
2014-10-14 17:10:05 +0300

    +bplustree.h

---
+++ BTreePImpl/BPlusTree.h
@@ -0,0 +1,54 @@
+#ifndef BPLUSTREE_H
+#define BPLUSTREE_H
+
+#include "List.h"
+#include <iostream>
+
+const int    D = 4;     // max count of elements in BTreeNode
+const double A = 0.5;   // A*D - min count of elements in BTreeNode
+
+struct BTreeNode {
+    BTreeNode *parent;      // ссылка на родителя
+    List children;          // список (делегат от дочернего нода (index), ссылка на него)
+    // rchild, вообще говоря, плохая идея. Лучше просто проверять наличие rbro, и 
+    // если его нет - не считать последнего делегата (отправлять по нему 
+    // при поиске/обходе). Тогда максимумом для нода
+    // будет D-1 ключей НЕ СЧИТАЯ ссылку на последний (Length: children.Length + (rbro ? 0:1))
+    // BTreeNode *rchild;      // ссылка на правый дочерний нод (не делегирует ключа)
+    BTreeNode *lbro, *rbro; // ссылки на левого и правого брата
+    bool isLeaf;            // является ли нода листом?
+    BTreeNode();
+    BTreeNode(const BTreeNode& orig);
+    int keysCount();
+    void draw(std::ostream& out);
+    DataType max(); 
+//    BTreeNode(DataType val, BTreeNode *parent = nullptr);
+};
+
+class BPlusTree {
+public:
+    BPlusTree();
+    BPlusTree(const BPlusTree& orig);
+    BPlusTree(BPlusTree&& orig);
+    BPlusTree& operator=(const BPlusTree& rv);
+    BPlusTree& operator=(BPlusTree&& rv);
+    ~BPlusTree();
+    
+    void insert(DataType val);
+    void remove(DataType val);
+    ListNode* find(DataType val);
+    void draw(std::ostream& out) const;
+    
+private:
+    BTreeNode *root;
+
+    void removeNode(BTreeNode* node);
+    // removes node and all its children
+    // IT JUST REALISE ALLOCATED MEMORY, IT DOESN'T CHANGE PARENT'S LINK
+    
+    void share(BTreeNode* lnode, BTreeNode* rnode); // перераспределение элементов соседних узлов
+    void split(BTreeNode* node);                    // разбиение одного узла на два
+    void fuse(BTreeNode* node1, BTreeNode* node2);  // объединение двух соседних узлов в один
+};
+
+#endif /* BPLUSTREE_H */
---
+++ BTreePImpl/List.h
@@ -0,0 +1,48 @@
+#ifndef LIST_H
+#define LIST_H
+
+#include <iostream>
+#define DataType int
+
+/*
+ * List - двусвязный список для B+деревьев
+ * Вставка происходит в отсортированный список (список отсортирован всегда)
+ */
+
+class BPlusTree;
+struct BTreeNode;
+
+struct ListNode {
+    ListNode *prev, *next;
+    DataType index;
+    BTreeNode *child;
+    ListNode();
+    ListNode(DataType val, BTreeNode* child);
+};
+
+class List {
+public:
+    List();
+    List(const List& orig);
+    List& operator=(List &orig);
+    List& operator=(List &&orig);
+    ~List();
+    
+    void insert(DataType val, BTreeNode* child = nullptr);
+    void remove(DataType val);
+    ListNode* find(DataType val);
+    int getLength() const;
+    void setLength(int len);
+
+    ListNode* begin();
+    ListNode* end();
+    void setBegin(ListNode* b);
+    void setEnd(ListNode* e);
+    
+private:
+    ListNode *_begin, *_end;
+    int length;
+    void clear();
+};
+
+#endif /* LIST_H */
