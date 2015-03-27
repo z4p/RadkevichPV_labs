7f8a83fee2a155ad18bb1bfa2d9984a002974271
z4p <z4p@tut.by>
2015-03-27 12:25:33 +0300

    Regex task performed

--- Java/src/lab3/regexp/RegExp.java
+++ Java/src/lab3/regexp/RegExp.java
@@ -1,8 +1,28 @@
 package lab3.regexp;
 
+import java.util.regex.Pattern;
+
+/*
+    Задание 1. Выполнил Радкевич Павел, 4 группа ММФ
+    20. В тексте найти первую подстроку максимальной длины, не содержащую букв.
+*/
 
 public class RegExp {
     public static void main(String[] args) {
+        String text = "...It is as so a thousand mouth cry out in pain! WRA!!!!! D'you hear these voices too?..";
+        String maxLine = "";
         
+        Pattern p = Pattern.compile("[^a-zA-Z]+", Pattern.MULTILINE);
+        int offset = 0;
+        for (int i = 0; i < text.length(); ++i) {
+            if (p.matcher(text.substring(offset, i)).matches()) {
+                if (i-offset > maxLine.length())
+                    maxLine = text.substring(offset, i);
+            } else {
+                offset = i;
+            }
+        }
+        
+        System.out.println("String: '" + text + "'\nSubstring: '" + maxLine + "'\nLength: " + maxLine.length());
     }
 }
